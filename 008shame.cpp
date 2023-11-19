3c3431777be7fedd2eca51f3ff41996516f3b546
CodingRodent <pn94958@users.noreply.github.com>
2023-11-19 23:39:30 +0100

    Meh.

---
+++ 008shame.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+void shame()
+{
+    cout << "I haven't learned anything from C++ today. I'm just writing this code to keep my coding streak going.\n";
+}
+
+int main()
+{
+    shame();
+}
